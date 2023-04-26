1f63ed0933c63fb72a53f0be19a102db1b55c327
BaddyM <101480645+BaddyM@users.noreply.github.com>
2023-04-27 00:26:57 +0300

    Third Commit

--- wedo.php
+++ wedo.php
@@ -1,7 +1,6 @@
 <?php
 $page = 'We do';
 
-
 include 'includes/header.php';
 include 'config/db.php';
 ?>
@@ -9,7 +8,7 @@ include 'config/db.php';
     <h5 class="text-center p-2 text-uppercase fw-bold text-decoration-underline animate__animated animate__bounce">What We do at Izuba Finance</h5>
     <p class="container text-center about">
         At Izuba finance, We provide all kinds of loans for every category of person for instance, teachers, students, drivers,
-        farmers....
+        farmers and any other person fit....
     </p>
 </div>
 
